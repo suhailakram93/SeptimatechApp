7f7507bee436d022f4f256346bb92a36fa853753
Vivek Katroju <Vkatroju5947@conestogac.on.ca>
2020-03-09 13:20:05 -0400

    first phase of functionality

---
+++ app/src/main/java/com/example/septimatechapp/AboutUs.java
@@ -0,0 +1,12 @@
+package com.example.septimatechapp;
+
+import android.os.Bundle;
+
+public class AboutUs extends basicMenu {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aboutus_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/septimatechapp/Categories.java
@@ -0,0 +1,11 @@
+package com.example.septimatechapp;
+
+import android.os.Bundle;
+
+public class Categories extends basicMenu {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.categories_main);
+    }
+}
