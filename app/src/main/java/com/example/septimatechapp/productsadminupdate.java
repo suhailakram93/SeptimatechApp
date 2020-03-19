6df43d6c9798c04ed005d412485b42e29341d36a
Himani <himanipatel13597@gmail.com>
2020-03-19 14:22:33 -0400

    push notification

--- app/src/main/java/com/example/septimatechapp/productsadminupdate.java
+++ app/src/main/java/com/example/septimatechapp/productsadminupdate.java
@@ -1,14 +1,14 @@
 package com.example.septimatechapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class adminHome extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class productsadminupdate extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_home);
+        setContentView(R.layout.activity_productsadminupdate);
     }
 }
