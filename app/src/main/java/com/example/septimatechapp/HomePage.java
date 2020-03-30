573732edf7afb356942e5abe01e78850f5d10830
Suhail Akram Mohammed <Smohammed0948@conestogac.on.ca>
2020-03-30 19:38:37 -0400

    added RXNT grid view

--- app/src/main/java/com/example/septimatechapp/HomePage.java
+++ app/src/main/java/com/example/septimatechapp/HomePage.java
@@ -58,7 +58,6 @@ public class HomePage extends basicMenu {
                 categorymenu.show();
             }
         });
-
     }
 }
 
---
+++ app/src/main/java/com/example/septimatechapp/Rxnt.java
@@ -0,0 +1,35 @@
+package com.example.septimatechapp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class Rxnt extends BaseAdapter {
+
+    private Context Rcontext;
+
+    public int[] imageArray = {
+
+    };
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
